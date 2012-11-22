db8a8b7b8019c780ebc76fd9b06d60f6f31349b6
Andrew Momzyakoff <andrius-smth@yandex.ru>
2012-11-22 17:25:53 +0400

    First version of bilingual internationalization
    -changed models and views
    - db backups
    - translation files

---
+++ mycms/upgr_topic.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE cms_blogentry_sav
+(
+  standardpage_ptr_id integer NOT NULL,
+  blog_id integer,
+  comment_allowed character varying(1) NOT NULL,
+  topic_id integer
+);
+
+insert into cms_blogentry_sav(standardpage_ptr_id, blog_id, comment_allowed, topic_id)
+select standardpage_ptr_id, blog_id, comment_allowed, topic_id from cms_blogentry;
+
+commit;
+
+insert into cms_blogentry(standardpage_ptr_id, blog_id, comment_allowed)
+select standardpage_ptr_id, blog_id, comment_allowed from cms_blogentry_sav;
+
+
+
+
+rollback;
